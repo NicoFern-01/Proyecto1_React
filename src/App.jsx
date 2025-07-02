373958bd6ff5119a2a5e0f6773301b117eea32ab
NicoFern-01 <invfer2020@gamail.com>
2025-07-02 08:29:24 -0300

    Inc-UserSatate

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
+import { useState } from 'react';
 import { Navbar } from './components/Navbar/Navbar';
 import { Footer } from './components/Footer/Footer';
-import { ItemListContainer } from './components/itemListContainer/ItemListContainer';
+import { ItemListContainer } from './components/ItemListContainer/ItemListContainer';
 
 function App() {
+  const [userName] = useState('Fernando'); // Nombre de usiario por defecto 
+
   return (
     <div>
       <Navbar />
       <main>
-        <ItemListContainer hola="¡Bienvenido a MiTienda!" />
+        <ItemListContainer 
+          hola="¡Bienvenido a MiTienda!" 
+          user={userName} 
+          subtitle="Acá encontrarás los mejores productos del mercado"
+        />
       </main>
       <Footer />
     </div>
--- src/components/itemListContainer/ItemListContainer.jsx
+++ src/components/itemListContainer/ItemListContainer.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
 
-export const ItemListContainer = ({ hola }) => {
+export const ItemListContainer = ({ hola, user, subtitle }) => {
   return (
     <section style={styles.container}>
       <h2>{hola}</h2>
-      {}
+      <h3>Hola, {user} 👋</h3>
+      <p>{subtitle}</p>
     </section>
   );
 };
